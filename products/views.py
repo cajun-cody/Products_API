23208c733bf7857abfac2d469567606873ac28d9
Cody <cemt5462@gmail.com>
2022-12-06 14:47:15 -0600

    Setup URL paths and started testing with postman

--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+
+@api_view(['GET','POST'])
+def products_list(request):
+
+
+    return Response('ok')
+
+
+@api_view(['GET'])
+def products_item(request,pk):
+
+
+    return Response(f'ok{pk}')
